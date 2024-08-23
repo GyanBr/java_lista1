57c3c0a8275b078920f18f0abba24cefdc6531ed
Gyanbr <trovao10000@hotmail.com>
2024-08-23 20:20:27 -0300

    Finalização Retangulo, criação +2 classes

--- src/Main/RetanguloMain.java
+++ src/Main/RetanguloMain.java
@@ -1,4 +1,13 @@
 package Main;
 
+import Model.Retangulo;
+
 public class RetanguloMain {
+    public static void main(String[] args) {
+        Retangulo retangulo = new Retangulo(3.5, 2.7);
+        System.out.println(retangulo);
+        System.out.println("Altura: " + String.format("%.2f", retangulo.getAltura())+ ".");
+        System.out.println("Largura: "+ String.format("%.2f", retangulo.getLargura())+ ".");
+        System.out.println("Area: " + String.format("%.2f", retangulo.calcularArea()) + ".");
+    }
 }
--- src/Model/Retangulo.java
+++ src/Model/Retangulo.java
@@ -5,5 +5,29 @@
 //    área e o perímetro do retângulo.
 package Model;
 public class Retangulo {
+    private double largura;
+    private double altura;
 
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double calcularArea() {
+        return altura * largura;
+    }
 }
