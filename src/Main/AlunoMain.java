b6270dee0f3b13363d808d6731482500150ee6fa
Gyanbr <trovao10000@hotmail.com>
2024-08-30 18:34:14 -0300

    classe aluno implementada

--- src/Main/AlunoMain.java
+++ src/Main/AlunoMain.java
@@ -3,6 +3,7 @@ package Main;
 import Model.Aluno;
 
 public class AlunoMain {
+    //a
     public static void main(String[] args) {
         Aluno aluno = new Aluno("Teste", "1234567", 1, 2, 3);
         System.out.println(aluno.toString());
